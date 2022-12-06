80aa13b380a66f11552ae9733a940438826473a7
Arthur <arthur020104@gmail.com>
2022-12-06 12:17:31 -0300

    adicionando ex1 e ex2 da atividade2

---
+++ Atv2/ex1.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int M,N;
+    printf("Digite a ordem das matrize A MxN\nM:");
+    scanf("%d",&M);
+    printf("N:");
+    scanf("%d",&N);
+    int **arr = (int **)malloc(M * sizeof(int*));
+    for(int i = 0; i < M; i++)
+    { 
+        arr[i] = (int *)malloc(N * sizeof(int));
+    }
+    for(int i=0; i < M; i++)
+    {
+        for(int j=0; j < N; j++)
+        {
+            printf("Matriz A M%dxN%d: ",i+1,j+1);
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int X,Y;
+    printf("Digite a ordem das matrize B MxN\nM:");
+    scanf("%d",&X);
+    printf("N:");
+    scanf("%d",&Y);
+    if(N != X)
+    {
+        printf("[ERROR:INVALID OPERATION] Impossivel mutiplicar matrizes dessas ordens.\n");
+        return 1;
+    }
+    int **arrb = (int **)malloc(X * sizeof(int*));
+    for(int i = 0; i < X; i++)
+    {
+        arrb[i] = (int *)malloc(Y * sizeof(int));
+    }
+    int **arrmult = (int **)malloc(M * sizeof(int*));
+    for(int i = 0; i < M; i++)
+    { 
+        arrmult[i] = (int *)malloc(Y * sizeof(int));
+    }
+
+    for(int i=0; i < X; i++)
+    {
+        for(int j=0; j < Y; j++)
+        {
+            printf("Matriz B M%dxN%d: ",i+1,j+1);
+            scanf("%d",&arrb[i][j]);
+        }
+    }
+
+        for(int j = 0; j<M;j++)
+        {
+            for(int h = 0; h<Y;h++)
+            {
+                int soma=0;
+                for(int i = 0;i<N;i++)
+                {
+                    soma += arr[j][i]*arrb[i][h];
+                }
+                arrmult[j][h] = soma;
+            }
+        }
+    printf("\nMatriz A:\n");
+    for(int i=0; i < M; i++)
+    {
+        for(int j=0; j < N; j++)
+        {
+            printf(" M%dxN%d: %d ",i+1,j+1,arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nMatriz B:\n");
+    for(int i=0; i < X; i++)
+    {
+        for(int j=0; j < Y; j++)
+        {
+            printf(" M%dxN%d: %d ",i+1,j+1,arrb[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nProduto matricial A.B:\n");
+    for(int i=0; i < M; i++)
+    {
+        for(int j=0; j < Y; j++)
+        {
+            printf(" M%dxN%d: %d ",i+1,j+1,arrmult[i][j]);
+        }
+        printf("\n");
+    }
+    free(arr);
+    free(arrb);
+    free(arrmult);
+    return 0;
+}
---
+++ Atv2/ex2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float main(){
+    printf("N:");
+    int N;
+    scanf("%d",&N);
+    float ***cubo = (float ***)malloc(N * sizeof(float**));
+    for(int i = 0; i < N; i++)
+    { 
+        cubo[i] = (float **)malloc(N * sizeof(float*));
+        for(int j = 0;j< N;j++)
+        {
+            cubo[i][j] = (float *)malloc(N * sizeof(float));
+            for(int h = 0;h< N;h++)
+            {
+                printf("Valor na posicao %dx%dx%d:",i+1,j+1,h+1);
+                float n;
+                scanf("%f",&n);
+                cubo[i][j][h] = n;
+            }
+        }
+    }
+    int F=0;
+    do
+    {
+        printf("\nEscolha um par:\n1-XY\n2-XZ\n3-YZ\n");
+        scanf("%d",&F);
+    }
+    while(F <= 0 || F >= 4 );
+    int X=0,Y=0,Z=0;
+    float sum=0;
+    if(F==1)
+    {
+        do
+        {
+            printf("X:");
+            scanf("%d",&X);
+            printf("Y:");
+            scanf("%d",&Y);
+        } while ((X <= 0 || X > N)|| (Y <= 0 || Y > N) );
+        
+        Y--;
+        X--;
+        for(Z=0;Z<N;Z++)
+        {
+            Z==(N-1) ? printf("%f",cubo[X][Y][Z]) : printf("%f+",cubo[X][Y][Z]);
+            sum += cubo[X][Y][Z];
+        }
+    }
+    else if(F==2)
+    {
+        do
+        {
+            printf("X:");
+            scanf("%d",&X);
+            printf("Z:");
+            scanf("%d",&Z);
+        }while((X <= 0 || X > N)|| (Z <= 0 || Z > N ));
+        X--;
+        Z--;
+        for(Y=0;Y<N;Y++)
+        {
+            Y==(N-1) ? printf("%f",cubo[X][Y][Z]) : printf("%f+",cubo[X][Y][Z]);
+            sum += cubo[X][Y][Z];
+        }
+    }
+    else
+    {
+        do
+        {
+            printf("Y:");
+            scanf("%d",&Y);
+            printf("Z:");
+            scanf("%d",&Z);
+        } while ((Y <= 0 || Y > N)|| (Z <= 0 || Z > N ));
+        Y--;
+        Z--;
+        for(X=0;X<N;X++)
+        {
+            X==(N-1) ? printf("%f",cubo[X][Y][Z]) : printf("%f+",cubo[X][Y][Z]);
+            sum += cubo[X][Y][Z];
+        }
+    }
+
+    printf("=%f",sum);
+    free(cubo);
+    return 0;
+}
