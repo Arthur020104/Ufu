8bcb9561019db6b6827fbde7a3de2ec40cf06477
Arthur020104 <83951969+Arthur020104@users.noreply.github.com>
2022-12-07 19:41:03 -0300

    adicionando exe4 da atv2

---
+++ Atv2/ex4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void main(void)
+{
+    char M[100] = "The quick brown fox jumps over the lazy dog";
+    char N[100] = "A ligeira raposa marrom saltou sobre o cachorro preguicoso";
+    char R[100];
+
+    //1) imprimir o tamanho das strings M e N
+
+    int lenM;
+    for(lenM=0;lenM<100;lenM++)
+    {
+        if(M[lenM]=='\0')
+        {
+            //lenM--;
+            break;
+        }
+    }
+    printf("\nTamanho da string M: %d",lenM);
+    int lenN;
+    for(lenN=0;lenN<100;lenN++)
+    {
+        if(N[lenN]=='\0')
+        {
+            //lenN--;
+            break;
+        }
+    }
+    printf("\nTamanho da string N:%d ",lenN);
+
+    // 2) copiar M para R
+    for(int j = 0;j< lenM;j++)
+    {
+        R[j]=M[j];
+        if(j == (lenM-1))
+        {
+            R[j+1]='\0';
+        }
+    }
+    printf("\n%s",R);
+
+    // 3) concatenar N em R
+    for(int i = 0;i<lenN;i++)
+    {
+        R[lenM+i] = N[i];
+    }
+     printf("\n%s",R);
+}
