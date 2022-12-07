9414caaa95ad7f37bb5870fb056367edecb02b30
Arthur020104 <83951969+Arthur020104@users.noreply.github.com>
2022-12-07 20:24:24 -0300

    Corrigindo os erros do ex3

--- Atv2/ex3.c
+++ Atv2/ex3.c
@@ -1,58 +1,34 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 const int STR_LEN = 30;
-const int STR_ARR_LEN = 20;
+const int STR_ARR_LEN = 5;
 
 int calculoordem(char s[]);
 
 int main()
 {
-    char string[20][30];
+    char string[5][30];
     for(int i =0;i<STR_ARR_LEN;i++)
     {
         scanf("%s",string[i]);
     }
-    for(int  i =0;i<STR_ARR_LEN;i++)
+    char tmp[30];
+    for(int i =0;i<STR_ARR_LEN;i++)
     {
-        for(int j = (STR_ARR_LEN-2);j>=0;j--)
+        tmp[i] =string[0][i]; 
+    }
+    for(int j = (STR_ARR_LEN-2);j>=0;j--)
+    {
+        if(strcmp(string[j+1],string[j])< 0)
         {
-            if(calculoordem(string[j])>calculoordem(string[j+1]))
+            for(int h =0;h<12;h++)
             {
-                char tmp[30];
-                for(int h =0;h<STR_LEN;h++)
-                {
-                    tmp[h] = string[j][h];
-                    string[j][h] = string[j+1][h];
-                    string[j+1][h] = tmp[h];
-                }
+                tmp[h] = string[j+1][h];
             }
         }
     }
-    printf("\n\nStrings ordenadas:");
-    for(int i =0;i<STR_ARR_LEN;i++)
-    {
-        printf("%s\n",string[i]);
-    }
+    printf("\n\n%s",tmp);
 }
-int calculoordem(char s[])
-{
-    int soma = 0;
-    for(int i=0;i<STR_LEN;i++)
-    {
-        if(s[i]=='\0')
-        {
-            break;
-        }
-        int c = (int)s[i];
-        if(c < 97)
-        {
-            soma += i==0 ? 10000 *(c+32) : (STR_LEN-i*2) <=0 ? 1 *(c+32): (STR_LEN-i*2) *(c+32);
-            continue;
-        }
-        soma += i==0 ? 10000 *(c) : (STR_LEN-i*2) <=0 ? 1 *(c): (STR_LEN-i*2) *(c);
-    }
-    return soma;
-}
-
