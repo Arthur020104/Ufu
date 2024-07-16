18251856447e36b11ec6214b840f857b8d00f6ef
Arthur <arthur020104@gmail.com>
2024-07-16 09:19:12 -0300

    Adicionando resolucao de soduko usando backtracking

--- Outros/soduko.py
+++ Outros/soduko.py
@@ -1,24 +1,68 @@
-from nRainhas import segura
+
 tabuleiro = []
-for i in range(8):
-    tabuleiro.append([])
-    for j in range(8):
-        tabuleiro[i].append(0)
-def Res(n,l):
-    if l > 7:
-        return True
-    for c in range(n):
-        if segura(tabuleiro,l,c):
-            tabuleiro[l][c] = 1
-            if Res(n,l+1):
+for i in range(9):
+    tabuleiro.append([0]*9)
+
+tabuleiro[0][0]=3
+tabuleiro[0][6]=8
+tabuleiro[1][1]=9
+tabuleiro[1][5]=6
+tabuleiro[2][2]=6
+tabuleiro[2][4]=2
+tabuleiro[2][6]=1
+tabuleiro[2][8]=7
+tabuleiro[3][0]=5
+tabuleiro[3][3]=4
+tabuleiro[4][2]=9
+tabuleiro[4][4]=1
+tabuleiro[4][6]=6
+tabuleiro[4][8]=2
+tabuleiro[5][7]=3
+tabuleiro[6][1]=4
+tabuleiro[6][3]=1
+tabuleiro[6][6]=7
+tabuleiro[6][8]=3
+tabuleiro[7][2]=7
+tabuleiro[7][7]=8
+tabuleiro[8][4]=3
+tabuleiro[8][7]=9
+
+def segura(c,l,n):
+    for i in range(9):
+        if tabuleiro[c][i] == n or tabuleiro[i][l] == n :
+            return False
+    instanceOfL = l - (l % 3)
+    instanceOfC = c - (c % 3)
+    for i in range(instanceOfC,instanceOfC+3):
+        for j in range(instanceOfL,instanceOfL+3):
+            if tabuleiro[i][j] == n :
+                return False
+        
+    return True
+def main(c,l):
+    if c >= 9:
+       return main(0, l + 1)
+   
+    if l >= 9:
+       return True
+   
+    if tabuleiro[c][l] != 0:
+        return main(c + 1, l)
+    
+    for i in range(1,10):
+        if segura(c,l,i):
+            tabuleiro[c][l] = i
+        
+            if main(c + 1, l):
                 return True
-            tabuleiro[l][c]=0
+        tabuleiro[c][l] = 0
+   
     return False
 
 if __name__ == "__main__":
-    for l in tabuleiro:
-        print(l)
+    for tab in tabuleiro:
+        print(tab)
     print()
-    Res(8,0)
-    for l in tabuleiro:
-        print(l)
+    main(0,0)
+    for tab in tabuleiro:
+        print(tab)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Tarefas e Atividades de Programação - UFU
+
+Este repositório contém tarefas e atividades de programação de várias disciplinas da Universidade Federal de Uberlândia (UFU), incluindo:
+
+- IPC (Introdução à Programação de Computadores)
+- ED1 (Estruturas de Dados I)
+- ED2 (Estruturas de Dados II)
+- Outras Disciplinas
+
+---
+
+# Programming Tasks and Activities - UFU
+
+This repository contains programming tasks and activities from various subjects at the Federal University of Uberlândia (UFU), including:
+
+- IPC (Introduction to Computer Programming)
+- ED1 (Data Structures I)
+- ED2 (Data Structures II)
+- Other Courses
